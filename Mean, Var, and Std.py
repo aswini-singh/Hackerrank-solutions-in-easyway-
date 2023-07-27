806e41447143de46dedc45e4eccc39622c1bd6ca
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-27 17:25:20 +0530

    adding new code

--- Mean, Var, and Std.py
+++ Mean, Var, and Std.py
@@ -1,5 +1,26 @@
-import numpy as np
-n, m = map(int, input().split())
-k = np.array([input().split() for _ in range(n)],dtype = int)
-np.set_printoptions(legacy='1.13')
-print(np.mean(k,axis=1), np.var(k,axis=0), np.std(k), sep='\n')
+import numpy
+nm = input()
+inm = nm.split( )
+inn = []
+for i in inm:
+    i = int(i)
+    inn.append(i)
+
+arr = []
+
+for i in range(0, inn[0]):
+    a = input()
+    k = []
+    b = a.split(" ")
+
+    for i in b:
+        i = int(i)
+        k.append(i)
+    arr.append(k)
+
+arn = numpy.array(arr)
+print(numpy.mean(arn, axis=1))
+print(numpy.var(arn, axis=0))
+z = numpy.std(arn, axis=None)
+j = round(z, 11)
+print(j)

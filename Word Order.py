47390d9b0f292a649752f1477e61a957c280ea81
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-27 16:39:26 +0530

    add new code

---
+++ Word Order.py
@@ -0,0 +1,12 @@
+# Word Order.py
+
+from collections import Counter, OrderedDict
+
+
+class OrderedCounter(Counter, OrderedDict):
+    pass
+
+
+ordered_counter = OrderedCounter(input() for _ in range(int(input())))
+print(len(ordered_counter))
+print(*ordered_counter.values())

6755eb786543b30e2fa423eb76e42e988de4f064
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-27 16:36:53 +0530

    adding

---
+++ Collections namedtuple().py
@@ -0,0 +1,7 @@
+# Collections namedtuple() 
+
+from collections import namedtuple
+
+
+n, Student = int(input()), namedtuple('Student', input())
+print("{:.2f}".format(sum([int(Student(*input().split()).MARKS) for _ in range(n)]) / n))

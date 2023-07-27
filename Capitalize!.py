ffc6c78c115febd32d544ecf7de3ca8bfa75ce4c
aswini-singh <aswiniamarsingh@gmail.com>
2023-07-27 17:43:39 +0530

    adding

---
+++ Capitalize!.py
@@ -0,0 +1,23 @@
+
+st = input()
+print(' '.join(word.capitalize() for word in st.split(' ')))
+
+
+# (or)
+
+# Complete the solve function below.
+def solve(s):
+    for x in s[:].split():
+        s = s.replace(x, x.capitalize())
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
